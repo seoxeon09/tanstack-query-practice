cea3c7c79da94824d0bed23e773b793b4214f2ef
서연 <s25037@gsm.hs.kr>
2025-12-26 18:16:52 +0900

    feat: main에 staleTime 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+      gcTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
